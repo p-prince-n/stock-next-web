6ead6e8e691f29946e7aedf4c516100780c62ce7
p-prince-n <pp2358296@gmail.com>
2025-10-27 23:03:02 +0530

    initial commit

---
+++ components/NavItems.tsx
@@ -0,0 +1,26 @@
+'use client';
+import { NAV_ITEMS } from '@/lib/constants'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import { use } from 'react'
+
+const NavItems = () => {
+    const pathName=usePathname()
+    const isActive=(path:string)=>{
+        if(path==='/') return pathName==='/';
+        return pathName.startsWith(path);
+    }
+  return (
+   <ul className='flex flex-col sm:flex-row p-2 gap-3 sm:gap-10 font-medium'>
+    {NAV_ITEMS.map(({href, title}, idx)=>(
+        <li key={idx}>
+            <Link href={href} className={`hover:text-yellow-500 transition-colors ${isActive(href)? 'text-gray-400': ''}`} >
+            {title}
+            </Link>
+        </li>
+    ))}
+   </ul>
+  )
+}
+
+export default NavItems
